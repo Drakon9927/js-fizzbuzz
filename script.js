3bf0ea47d6dc011a6f5b6065ef86a44f2a05a24e
Drakon9927 <drakon9927@libero.it>
2024-04-19 15:03:53 +0200

    fizzbuzz

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+for (i = 1; i <= 10000; i++) {
+    
+    let output = i;
+
+    if (i % 3 === 0 && i % 5 === 0) {
+        output = 'fizzbuzz';
+        console.log('fizzbuzz');
+    } else if (i % 3 === 0) {
+        output = 'fizz';
+        console.log('fizz');
+    } else if (i % 5 === 0) {
+        output = 'buzz';
+        console.log('buzz');
+    } else {
+        console.log(i);
+    }
+}
+
